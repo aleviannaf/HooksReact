18b5576016ceead9de11585dd9edbdf15c97119d
aleviannaf <alexandrevianna511@gmail.com>
2022-10-24 21:49:08 -0400

    feat(useEffect): added useEffect

--- src/pages/UseEffectPage/index.jsx
+++ src/pages/UseEffectPage/index.jsx
@@ -1,13 +1,47 @@
+import { useEffect, useState } from "react";
+
+const UseEffectPage = () => {
+  const [resourceType, setResourceType] = useState("post");
+
+  const changeResourceType = (resourceType) => {
+    setResourceType(resourceType);
+  };
+
+  useEffect(() => {
+
+    const fetchResourceTypes = async () => {
+      
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/${resourceType}`
+      );
+    
+      const responseJSON = await response.json();
+      console.log(responseJSON);
+    };
+
+    fetchResourceTypes();
+  },[resourceType]);
 
 
-const UseEffectPage = ()=>{
-    return(
-        <div>
-            <h2>
-                useState
-            </h2>
-        </div>
-    )
-}
+  useEffect(()=>{
+    //compenentDidMount
+    console.log("componentDidMount");
+    //compenentWillUnMount
+    return()=>{
+        console.log("componetWillUnmount")
+    }
+  })
+
+  return (
+    <div>
+      <h2>{resourceType}</h2>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <button onClick={() => changeResourceType("posts")}>Posts</button>
+        <button onClick={() => changeResourceType("comments")}>Comments</button>
+        <button onClick={() => changeResourceType("todos")}>Todos</button>
+      </div>
+    </div>
+  );
+};
 
 export default UseEffectPage;
